fddd88ecf21678bc8ee64d1c50fbee28e5f7d3e8
Ryan DeLong <rdelong111@gmail.com>
2022-05-29 21:24:53 -0500

    commit tree ef4a214d90b704a96ec8902445553c115c0d2b3b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last = "and #{array.pop}"
+    array << last
+    array.join(", ")
+  end
 end
